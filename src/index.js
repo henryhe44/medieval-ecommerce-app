c3de6bddbcfa760c9c5a7fc2c9d105cd5f30560e
unknown <hhe@mailfence.com>
2020-08-04 22:09:40 -0400

    add themeProvider (modify colors & tweak sizes later)

--- src/index.js
+++ src/index.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { ThemeProvider } from "styled-components";
 import './index.css';
 import App from './App';
+import theme from "./theme";
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
-  <App />,
+  <ThemeProvider theme={theme}>
+    <App />
+  </ThemeProvider>,
   document.getElementById('root')
 );
 
